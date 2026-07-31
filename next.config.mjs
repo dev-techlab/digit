369c4caf2784d5b5ed3352cf6a62619166a8c4a4
dev-techlab <dev.techlabsolution@gmail.com>
2026-07-31 22:02:21 +0530

    feat: add utilities for downloading remote assets and migrating database image references to local relative paths

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'static.digitlink.mobi',
+        hostname: 'octanlink.com',
       },
     ],
   },
